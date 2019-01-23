422915438ad873a0ceaca035bf473a8412bd8b2f
tronxi <sergio.gsanchez97@gmail.com>
2019-01-23 20:43:36 +0100

    emitir cambios a salas correctas

--- app.js
+++ app.js
@@ -63,7 +63,6 @@ io.on('connect', function(socket) {
         //io.sockets.in(data).emit('message', 'socket');
     });
     socket.on('iniciar', function(data){
-        console.log(data);
         cl.getDatos(function(error, resultado){
             if(error){
                 throw error;
@@ -75,7 +74,7 @@ io.on('connect', function(socket) {
                 x: resultado[i].x,
                 y:resultado[i].y});
             }
-            socket.emit('recibir-datos-iniciales', obj);
+            socket.emit.in(data)('recibir-datos-iniciales', obj);
         }, data)
         
     });

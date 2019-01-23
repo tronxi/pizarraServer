dc4f51f1b679204de2e7ba59fcc696be3581dd49
tronxi <sergio.gsanchez97@gmail.com>
2019-01-23 19:29:46 +0100

    crear

--- node/consultas.js
+++ node/consultas.js
@@ -20,12 +20,26 @@ exports.comprobarPizarra = function(cb, pizarraNombre) {
 }
 
 exports.crearPizarra = function(cb, pizarraNombre) {
-    let qr = "insert into pizarra ("+pizarraNombre+", antx, anty, x, y) values ('prueba', -1, -1, -1, -1)";
+    let qr = "select nombre from pizarra";
     bd.query(qr, function (error, filas){
         if(error) {
-            console.log('error al crear pizarras');
+            console.log('error al comprobar pizarras');
             return;
         }
-        cb(error, "creada");
+        for(let i = 0; i < filas.length; i++) {
+            if(filas[0].nombre == pizarraNombre)
+            {
+                cb(error, "existe");
+                return;
+            }
+        }
+        let qr = "insert into pizarra ("+pizarraNombre+", antx, anty, x, y) values ('prueba', -1, -1, -1, -1)";
+        bd.query(qr, function (error, filas){
+            if(error) {
+                console.log('error al crear pizarras');
+                return;
+            }
+            cb(error, "creada");
+        });
     });
 }

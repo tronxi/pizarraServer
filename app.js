65189ef271194b9dfdd3c87e93ab1720ea97a164
tronxi <sergio.gsanchez97@gmail.com>
2019-01-24 01:41:26 +0100

    avisar a todos

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ io.on('connect', function(socket) {
                 color: resultado[i].color,
                 tam: resultado[i].tam});
             }
-            socket.emit('recibir-datos-iniciales', obj);
+            socket.in(data).emit('recibir-datos-iniciales', obj);
         }, data)
         
     });
